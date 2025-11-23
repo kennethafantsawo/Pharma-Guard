f782f85b3a5d458c945f97139d68bee73b6a9786
kenneth afantsawo <kennethafantsawo@gmail.com>
2025-11-23 23:40:06 +0000

    http://localhost:3000/?error=invalid_request&error_code=bad_oauth_state&

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -10,7 +10,7 @@ export function createSupabaseServerClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
+       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
         },
@@ -33,6 +33,10 @@ export function createSupabaseServerClient() {
           }
         },
       },
+      //This is the important part, it tells Supabase to not store the session in memory.
+      auth: {
+        storage: null,
+      }
     }
   )
 }
