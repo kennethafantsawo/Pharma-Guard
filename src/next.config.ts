dead3b4de88614afce3277ed80baed4b86115f9a
kenneth afantsawo <kennethafantsawo@gmail.com>
2025-12-14 11:39:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -30,6 +30,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 // Add Supabase storage hostname if the URL is set
